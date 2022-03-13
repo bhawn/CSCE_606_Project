b34c3da5e46873d2aecfcdfa116ca1c9a6eac74d
mjulian <mjulian@tamu.edu>
2022-03-13 13:25:09 -0500

    Testing

--- dist/game.f6827abd.js
+++ dist/game.f6827abd.js
@@ -5333,7 +5333,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49939" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50777" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
