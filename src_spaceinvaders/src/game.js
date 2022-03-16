5468d6e410cf201eebcf361218531dd181f11949
kaushal <kausht14@gmail.com>
2022-03-16 11:05:21 -0500

    done with initial draft

--- src_spaceinvaders/src/game.js
+++ src_spaceinvaders/src/game.js
@@ -1,6 +1,12 @@
 import kaboom from "../../node_modules/kaboom";
 import { playableMap } from "./PlayableMap";
 const MOVE_SPEED = 200;
+
+const INVADER_SPEED = 200;
+let CURRENT_SPEED = INVADER_SPEED;
+const LEVEL_DOWN = 30;
+const BULLET_SPEED = 400;
+const TIME_LEFT = 30;
 const k = kaboom({
   global: true,
   // enable full screen
@@ -14,9 +20,9 @@ const k = kaboom({
 });
 
 loadRoot("https://i.imgur.com/");
-loadSprite("space-invader", "m2A06Eg.png"); // https://imgur.com/m2A06Eg
+loadSprite("space_invader", "m2A06Eg.png"); // https://imgur.com/m2A06Eg
 loadSprite("wall", "gqVoI2b.png");
-loadSprite("space-ship", "Wb1qfhK.png");
+loadSprite("space_ship", "Wb1qfhK.png");
 
 scene("game", ({ level, score }) => {
   //create layers
@@ -33,9 +39,9 @@ scene("game", ({ level, score }) => {
     // parameters 1: name of the sprite, 2: solid , 3: tag
 
     // load in some sprites
-    "^": () => [sprite("space-invader"), scale(0.7)],
-    "!": () => [sprite("wall", "left-wall")],
-    "&": () => [sprite("wall", "right-wall")],
+    "^": () => [sprite("space_invader"), scale(0.7), "space_invader", area()],
+    "!": () => [sprite("wall"), "left_wall", area()],
+    "&": () => [sprite("wall"), "right_wall", area()],
   };
 
   const gameLevel = addLevel(playableMap[level], levelCfg);
@@ -66,10 +72,30 @@ scene("game", ({ level, score }) => {
     fixed(),
   ]);
 
+  const timer = add([
+    text("0"),
+    pos(240, 38),
+    scale(0.3),
+    layer("ui"),
+    fixed(),
+    { time: TIME_LEFT },
+  ]);
+
+  add([text("Time Remaining: "), pos(20, 38), scale(0.3), fixed()]);
+  onUpdate(() => {
+    (timer.time -= dt()), (timer.text = timer.time.toFixed(2));
+
+    if (timer.time <= 0) {
+      go("lose", { score: scoreLabel.value });
+    }
+  });
+
   const player = add([
-    sprite("space-ship"),
+    sprite("space_ship"),
     pos(width() / 2, height() / 2),
     origin("center"),
+    area(),
+    solid(),
   ]);
 
   keyDown("left", () => {
@@ -78,6 +104,83 @@ scene("game", ({ level, score }) => {
 
   keyDown("right", () => {
     player.move(MOVE_SPEED, 0);
+  });
+
+  function spawnBullet(p) {
+    add([
+      rect(2, 10),
+      pos(p),
+      origin("center"),
+      color(0.5, 0.5, 1),
+      "bullet",
+      area(),
+    ]);
+  }
+  keyPress("b", () => {
+    spawnBullet(player.pos.add(0, -25));
+  });
+
+  onUpdate("bullet", (b) => {
+    b.move(0, -BULLET_SPEED);
+    if (b.pos.y < 0) {
+      destroy(b);
+    }
+  });
+
+  collides("bullet", "space_invader", (b, s) => {
+    shake(4);
+    destroy(b);
+    destroy(s);
+    scoreLabel.value++;
+
+    // then display the score
+    scoreLabel.text = scoreLabel.value;
+  });
+  //Let us make the space_invader moving
+  onUpdate("space_invader", (s) => {
+    if (s != null) {
+      s.move(CURRENT_SPEED, 0);
+    } else {
+      go("game", { level: level + 1, score: scoreLabel.value });
+    }
+  });
+  //On Collision with right wall
+  // Space-invader has to turn around and move down on each collision
+  collides("space_invader", "right_wall", () => {
+    CURRENT_SPEED = -INVADER_SPEED;
+
+    every("space_invader", (s) => {
+      s.move(0, LEVEL_DOWN);
+    });
+  });
+
+  collides("space_invader", "left_wall", () => {
+    CURRENT_SPEED = INVADER_SPEED;
+    every("space_invader", (s) => {
+      s.move(0, LEVEL_DOWN);
+    });
+  });
+
+  player.on("space_invader", () => {
+    go("lose", { score: scoreLabel.value });
+  });
+
+  onUpdate("space_invader", (s) => {
+    if (s.pos.y >= height() / 2) {
+      go("lose", { score: scoreLabel.value });
+    }
+  });
+});
+
+scene("lose", ({ score }) => {
+  add([text(score, 32), origin("center"), pos(width() / 2, height() / 2)]);
+  add([
+    text('Hit "Space bar" to Play again'),
+    scale(0.5),
+    pos(width() / 2 - 240, height() / 2 + 30),
+  ]);
+  onKeyPress("space", () => {
+    go("game", { level: 0, score: 0 });
   });
 });
 
