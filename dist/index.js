32ce2cbe53eb24fd77924a95f2de2e45fabbd7a0
mjulian <mjulian@tamu.edu>
2022-04-07 08:50:32 -0500

    Remove body from covid

--- dist/index.js
+++ dist/index.js
@@ -212,7 +212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51630" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52128" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src_mario/src/game.js
+++ src_mario/src/game.js
@@ -312,7 +312,7 @@ scene("game", ({ level, score }) => {
 
     "+": () => [sprite("pipe-top-right"), solid(), scale(0.5), "pipe", area()],
 
-    "^": () => [sprite("covid"), "dangerous", body(), area()],
+    "^": () => [sprite("covid"), scale(1), "dangerous", area()],
 
     "#": () => [sprite("mushroom"), solid(), "mushroom", body(), area()],
 
