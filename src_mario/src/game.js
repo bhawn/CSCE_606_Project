0c19148998d74ce8f32e84507e26173d36d1613a
mjulian <mjulian@tamu.edu>
2022-04-18 18:36:42 -0500

    Only use scaling for mobile buttons.

--- src_mario/src/game.js
+++ src_mario/src/game.js
@@ -7,8 +7,8 @@ const MOVE_SPEED = 150;
 const JUMP_FORCE = 560;
 const BIG_JUMP_FORCE = 750;
 let CURRENT_JUMP_FORCE = JUMP_FORCE;
-let BUTTON_YPOS = window.innerHeight-175;
-let BUTTON_FAR_XPOS = window.innerWidth-50;
+let BUTTON_YPOS = window.innerHeight-475;
+let BUTTON_FAR_XPOS = window.innerWidth-750;
 const ENEMY_SPEED = 20;
 let isJumping = true;
 const FALL_DEATH = 700;
@@ -27,7 +27,7 @@ const k = kaboom({
   // enable full screen
   fullscreen: true,
 
-  scale: 1,
+  scale: 2,
   background: [0, 0, 1],
   clearColor: [255, 255, 255],
   // for debug mode
@@ -147,7 +147,10 @@ window.mobileAndTabletCheck = function() {
   let check = false;
   (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
   console.log("Check is " + check);
-  return check;
+  if(check) {
+	  return 3;
+  }
+  return 1;
 };
 
 function resize() {
@@ -164,7 +167,8 @@ function resize() {
     canvas.style.width = windowHeight * gameRatio + "px";
     canvas.style.height = windowHeight + "px";
   }
-  BUTTON_YPOS = window.innerHeight-75;
+  BUTTON_YPOS = window.innerHeight-475;
+  BUTTON_FAR_XPOS = window.innerWidth-750;
 }
 
 //add scenes
@@ -232,7 +236,7 @@ scene("game", ({ level, score }) => {
   // background layer, object layer as default, UI layer
   // initialise with obj as default
   if(window.mobileAndTabletCheck()){
-	BASE_SCALE = 3;
+	BASE_SCALE = 2;
   }
   layers(["bg", "obj", "ui"], "obj");
   
@@ -279,24 +283,24 @@ scene("game", ({ level, score }) => {
     //Newly added Sprites end here
 
     "}": () => [sprite("unboxed"), solid(), area()],
-    "(": () => [sprite("pipe-bottom-left"), solid(), scale(BASE_SCALE*0.5), "pipe", area()],
+    "(": () => [sprite("pipe-bottom-left"), solid(), scale(0.5), "pipe", area()],
 
-    ")": () => [sprite("pipe-bottom-right"), solid(), scale(BASE_SCALE*0.5), "pipe", area()],
+    ")": () => [sprite("pipe-bottom-right"), solid(), scale(0.5), "pipe", area()],
 
-    "-": () => [sprite("pipe-top-left"), solid(), scale(BASE_SCALE*0.5), "pipe", area()],
+    "-": () => [sprite("pipe-top-left"), solid(), scale(0.5), "pipe", area()],
 
-    "+": () => [sprite("pipe-top-right"), solid(), scale(BASE_SCALE*0.5), "pipe", area()],
+    "+": () => [sprite("pipe-top-right"), solid(), scale(0.5), "pipe", area()],
 
-    "^": () => [sprite("covid"), scale(BASE_SCALE*1), "dangerous", area()],
+    "^": () => [sprite("covid"), scale(1), "dangerous", area()],
 
-    "#": () => [sprite("mushroom"), solid(), scale(BASE_SCALE*1), "mushroom", body(), area()],
+    "#": () => [sprite("mushroom"), solid(), scale(1), "mushroom", body(), area()],
 
     o: () => [
       sprite("BigVaccineMushroom"),
       solid(),
       "BigVaccineMushroom",
       body(),
-	  scale(BASE_SCALE*1),
+	  scale(1),
       area(),
 
       scale(0.1, 0.1),
@@ -306,18 +310,18 @@ scene("game", ({ level, score }) => {
       solid(),
       "BulletVaccineMushroom",
       body(),
-	  scale(BASE_SCALE*1),
+	  scale(1),
       area(),
       scale(0.1, 0.1),
     ],
 
-    "!": () => [sprite("blue-block"), solid(), scale(BASE_SCALE*0.5), area()],
-    "£": () => [sprite("blue-brick"), solid(), scale(BASE_SCALE*0.5), area(), "brick"],
+    "!": () => [sprite("blue-block"), solid(), scale(0.5), area()],
+    "£": () => [sprite("blue-brick"), solid(), scale(0.5), area(), "brick"],
 
     z: () => [
       sprite("blue-evil-shroom"),
       // solid(),
-      scale(BASE_SCALE*1),
+      scale(1),
       // body(),
       area(),
       "dangerous"
@@ -326,10 +330,10 @@ scene("game", ({ level, score }) => {
       sprite("blue-surprise"),
       solid(),
       area(),
-      scale(BASE_SCALE*0.5),
+      scale(0.5),
       "coin-surprise",
     ],
-    x: () => [sprite("blue-steel"), solid(), area(), scale(BASE_SCALE*1)],
+    x: () => [sprite("blue-steel"), solid(), area(), scale(1)],
   };
   // now just create a  gamelevel(JS method) and pass the map and levelCfg
   const gameLevel = addLevel(playableMap[level], levelCfg);
@@ -337,12 +341,12 @@ scene("game", ({ level, score }) => {
   // default layer is 'obj '
   // so change layer to 'ui' for adding score
   //define this as a method so that it can be passed to other levels
-  add([text("Score:"), scale(BASE_SCALE*0.3), pos(20, 6), fixed()]);
+  add([text("Score:"), scale(0.3), pos(20, 6), fixed()]);
   const scoreLabel = add([
     //text(score),
     text(parseInt(score)),
     pos(115, 6),
-    scale(BASE_SCALE*0.3),
+    scale(0.3),
     fixed(),
     layer("ui"),
     {
@@ -355,7 +359,7 @@ scene("game", ({ level, score }) => {
   add([
     text("Level: " + parseInt(level + 1)),
     pos(20, 22),
-    scale(BASE_SCALE*0.3),
+    scale(0.3),
     fixed(),
   ]);
 
@@ -403,7 +407,7 @@ scene("game", ({ level, score }) => {
     body(),
     big(),
     area(),
-	scale(BASE_SCALE*1),
+	scale(1),
     origin("bot"),
   ]);
 
@@ -692,7 +696,7 @@ scene("game", ({ level, score }) => {
 		  pos(p),
 		  origin("center"),
 		  color(255, 0.5, 1),
-		  scale(BASE_SCALE*1),
+		  scale(1),
 		  "bullet",
 		  area(),
 		]);
@@ -757,34 +761,34 @@ scene("game", ({ level, score }) => {
       pos(25, BUTTON_YPOS),
       opacity(0.5),
       fixed(),
-	  scale(BASE_SCALE*3),
+	  scale(BASE_SCALE),
       area(),
     ]);
 
     const rightButton = add([
       sprite("d"),
-      pos(25 + BASE_SCALE*175, BUTTON_YPOS),
+      pos(25 + BASE_SCALE*60, BUTTON_YPOS),
       opacity(0.5),
       fixed(),
-	  scale(BASE_SCALE*3),
+	  scale(BASE_SCALE),
       area(),
     ]);
 
     const actionButton = add([
       sprite("highjump"),
-      pos(BUTTON_FAR_XPOS-75, BUTTON_YPOS),
+      pos(BUTTON_FAR_XPOS-100, BUTTON_YPOS),
       opacity(0.5),
       fixed(),
-	  scale(BASE_SCALE*3),
+	  scale(BASE_SCALE),
       area(),
     ]);
 
     const shootButton = add([
       sprite("shoot"),
-      pos(BUTTON_FAR_XPOS-(375*BASE_SCALE), BUTTON_YPOS+(45*BASE_SCALE)),
+      pos(BUTTON_FAR_XPOS-(200*BASE_SCALE), BUTTON_YPOS+(20*BASE_SCALE)),
       opacity(0.5),
       fixed(),
-	  scale(BASE_SCALE*3),
+	  scale(BASE_SCALE),
       area(),
     ]);
 
@@ -870,10 +874,18 @@ scene("game", ({ level, score }) => {
 scene("lose", ({ score }) => {
   add([text(score, 32), origin("center"), pos(width() / 2, height() / 2)]);
   add([
-    text("Game Over. Going Back to Main Menu in 2 seconds"),
+    text("Game Over."),
     color(200, 50, 10),
     scale(0.5),
-    pos(window.innerWidth / 3 - 300, window.innerHeight / 2 + 30),
+	origin("center"),
+    pos(width() / 2, height() / 2 - 120),
+  ]);
+  add([
+    text("Going Back to Main Menu"),
+    color(200, 50, 10),
+    scale(0.5),
+	origin("center"),
+    pos(width() / 2, height() / 2 - 80),
   ]);
 
   // start the game
