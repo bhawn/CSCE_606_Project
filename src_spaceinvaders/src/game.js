17ec2fa51149d3f1ddb63bd15799c00eab6a74b4
drake205 <drake205@live.com>
2022-04-24 12:02:47 -0500

    Adjusted Maps to better center enemies

--- src_spaceinvaders/src/game.js
+++ src_spaceinvaders/src/game.js
@@ -8,9 +8,8 @@ const INVADER_SPEED = 100;
 let INVADER_DIRECTION = 1;
 let CURRENT_SPEED = INVADER_SPEED;
 let INVADER_MOVE_COUNT = 0;
-const LEVEL_DOWN = 400;
+const LEVEL_DOWN = 1000;
 const BULLET_SPEED = 400;
-const TIME_LEFT = 30000;
 const LIVES_REMAINING = 4;
 let BUTTON_YPOS = window.innerHeight - 125;
 let BASE_SCALE = 2;
@@ -292,26 +291,6 @@ scene("game", ({ level, score }) => {
     fixed(),
   ]);
 
-  // Timer for each level
-
-  const timer = add([
-    text("0"),
-    pos(240, 38),
-    scale(0.3),
-    layer("ui"),
-    fixed(),
-    { time: TIME_LEFT },
-  ]);
-
-  add([text("Time Remaining: "), pos(20, 38), scale(0.3), fixed()]);
-  onUpdate(() => {
-    (timer.time -= dt()), (timer.text = timer.time.toFixed(2));
-
-    if (timer.time <= 0) {
-      go("lose", { score: scoreLabel.value });
-    }
-  });
-
   const player = add([
     sprite("space_ship"),
     pos(width() / 2 - 500, height() / 2),
@@ -513,7 +492,7 @@ scene("game", ({ level, score }) => {
     if (enemyCount === 0) {
       //Invaders always move right at start of level
       INVADER_DIRECTION = 1;
-
+      INVADER_MOVE_COUNT = 0;
       // level = level + 1;
       // console.log("map count: " + playableMap.length);
       if (playableMap.length > level + 1) {
